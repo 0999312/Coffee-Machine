e2f05b346c961a9b4640402d6721d050eb0796cd
unknown <617510630@qq.com>
2022-01-23 02:52:44 +0800

    PROBLEMS.md用于记录开发过程中的问题

--- src/include/functions.h
+++ src/include/functions.h
@@ -1,7 +1,6 @@
 /*
  * 请把经常需要调用的函数（除structs.h里面封装好的增删改查之外）写在这个头文件里。
  */
-
 #ifndef INCLUDE_FUNCTIONS_H_
 #define INCLUDE_FUNCTIONS_H_
 /*
@@ -9,6 +8,10 @@
  * #ifndef表示如果未定义头文件则进行一次定义。
  * 这个写法来自于C，而且C++也需要使用这种方法。
  */
+#include <iostream> // C++ 风格输入输出，使用输入输出流进行操作。
+#include <utility> // C++ 通用工具头文件，pair定义于此
+#include <list> //C++ STL实现 双向链表（我们甚至不需要自己写链表了，YES）
+#include "include/structs.h" // 自己的结构体头文件
 
 
 
